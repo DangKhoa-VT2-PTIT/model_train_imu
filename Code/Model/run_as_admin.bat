1adf9e7ef806347d43af03ff8574856969a14584
DangKhoa-VT2-PTIT <125228780+DangKhoa-VT2-PTIT@users.noreply.github.com>
2024-10-08 11:12:36 +0700

    Delete Code directory

--- Code/Model/run_as_admin.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set script_path="C:\Users\ADMIN\Desktop\model_train_imu\DoAn_He_Thong_Nhung\Code\Model\Train_Model.py"
-echo Running %script_path% with admin privileges.
-powershell -Command "Start-Process python -ArgumentList '%script_path%' -Verb RunAs"
-pause
